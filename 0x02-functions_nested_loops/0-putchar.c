5b28e0b85402b7fdb04972292b39834f0303f12a
root <naisongareth2@gmail.com>
2023-08-16 12:40:13 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
-void _putchar(char c) {
-    putchar(c);
-}
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
 
-int main() {
-    _putchar('putchar',\n');
-
-    return 0;
-}
